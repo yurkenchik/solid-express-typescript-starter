b02de7302fc2508c3e8b23dfea5bbf5f4d5f8c6b
yurkenchik <yura.ilchyshyn06@gmail.com>
2025-08-23 13:12:48 +0300

    Added database configuration logic

--- src/infrastructure/database/DatabaseService.ts
+++ src/infrastructure/database/DatabaseService.ts
@@ -1,7 +1,21 @@
-import { SingletonImplementation } from "../../common/SingletonImplementation";
+import { SingletonImplementation } from "@common/SingletonImplementation";
+import { ConfigService } from "../config/ConfigService";
+import { Logger } from "@infrastructure/logger/Logger";
+import * as mongoose from "mongoose";
 
 export class DatabaseService extends SingletonImplementation<DatabaseService> {
+    private readonly configService: ConfigService;
+    private readonly logger: Logger;
+
     constructor() {
         super();
+        this.configService = ConfigService.getInstance();
+        this.logger = Logger.getInstance();
+    }
+
+    async connect(): Promise<void> {
+        const mongoDatabaseUrl = this.configService.getMongoUri();
+        await mongoose.connect(mongoDatabaseUrl);
+        this.logger.info('Mongo database successfully connected');
     }
 }
