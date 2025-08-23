c5ae1b23e0292dc2b98c9218e48e43c3b76a1a80
yurkenchik <yura.ilchyshyn06@gmail.com>
2025-08-23 13:11:37 +0300

    Changed request logger middleware

--- src/common/middlewares/requestLoggerMiddleware.ts
+++ src/common/middlewares/requestLoggerMiddleware.ts
@@ -1,14 +1,13 @@
 import { Request, Response, NextFunction } from "express";
-import { Logger } from "../infrastructure/logger/Logger";
-import { NextFunction } from "express";
+import { Logger } from "@infrastructure/logger/Logger";
 
 export function requestLoggerMiddleware(request: Request, response: Response, next: NextFunction) {
     const logger: Logger = Logger.getInstance();
 
-    logger.info(`Incoming request: ${request.method} ${request.originalUrl}`);
+    logger.request(`Incoming request: ${request.method} ${request.originalUrl}`);
 
     response.on('finish', () => {
-        logger.info(`Response status: ${response.statusCode} for ${request.method} ${request.originalUrl}`);
+        logger.response(`Response status: ${response.statusCode} for ${request.method} ${request.originalUrl}`);
     });
 
     next();
